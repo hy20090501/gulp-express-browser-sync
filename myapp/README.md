2e1785a4e8931af9be8be8c9aac3a1f4859647a8
hy20090501 <hy20090501@163.com>
2016-07-01 10:59:09 +0800

    a app demo based on express and browser-sync
    support browser reloading

---
+++ myapp/README.md
@@ -0,0 +1,19 @@
+#Express+Gulp+BrowserSync+nodemon
+Run an Express app base on browserSync proxy, you can enjoy browser reloading.
+
+### Installation
+
+To install the stable version:
+
+```
+cd myapp
+npm install --save redux
+```
+
+
+Then run `gulp` to start
+
+  * The Express server will run at localhost: 3001
+  * The BrowserSync will run at localhost: 3002
+
+Browser to localhost:5000 ,go dev!
---
+++ myapp/gulpfile.js
@@ -0,0 +1,39 @@
+// var ejs = require('gulp-ejs');
+// var gutil = require('gulp-util');
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+var nodemon = require('gulp-nodemon');
+
+gulp.task('browser-sync', ['express-server'], function() {
+    browserSync.init({
+        proxy: "localhost:3000",
+        open: false,
+        port: 5000,
+    });
+    //监听模板变化
+    gulp.watch([
+        './views/*.ejs'
+    ], function() { reload(); });
+
+});
+
+
+gulp.task('express-server', function() {
+    nodemon({
+        script: 'app.js',
+        ext: './views/*.ejs html',
+        env: { 'NODE_ENV': 'development' }
+    })
+});
+
+gulp.task('default', ['browser-sync'], function() {
+
+});
+// gulp.task('ejs2html', function() {
+//     gulp.src('./views/*.ejs')
+//         .pipe(ejs({
+//             msg: 'Hello Gulp!'
+//         }).on('error', gutil.log))
+//         .pipe(gulp.dest('./dist'));
+// });
